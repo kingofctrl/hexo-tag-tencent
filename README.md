63ec8e24650a9b0098bebcc9541c7d0ec5b90845
HmyBmny <hmybmny@gmail.com>
2016-06-24 23:33:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# hexo-tag-video_cn
+
+## 安装
+```
+npm install hexo-tag-video_cn 
+```
+
+## 文档
+
+https://hmybmny.github.io/hexo-theme-concise/2016/06/20/video/
+
+项目的灵感来源于hexo-tag-xvideos
+
+## 感谢
+
+![hexo-tag-xvideos](https://github.com/welksonramos/hexo-tag-xvideos)  
+之前我一直觉得除了WIFI万能钥匙以外再没有一件神器能让我如此心动，有了它，月入百万你也能行！
---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * hexo-tag-video_cn
+ * https://github.com/HmyBmny/hexo-tag-video_cn
+ * Copyright (c) 2016, HmyBmny
+ * Licensed under the MIT license.
+ */
+
+hexo.extend.tag.register('video', function(args){
+    video_provider = args[0];
+    var tag = ''
+    var base_url = '',
+    video_id = args[1];
+    width = args[2];
+    height = args[3];
+    switch (video_provider) {
+        case 'youku':
+            base_url = 'http://player.youku.com/embed/',
+            tag = '<iframe height=' + height  +' width=' + width +' src="' + base_url + video_id +'" frameborder=0 allowfullscreen></iframe>'
+            break;
+        case 'v_qq':
+            var base_url = 'http://v.qq.com/iframe/player.html?vid=',
+            tag = '<iframe frameborder="0" width="' + width +'" height="'+ height +'" src="' + base_url + video_id +'&tiny=0&auto=0" allowfullscreen></iframe>'
+            break;
+        default:
+            tag = '<center><font color=red size=2>插入视频出错，<a href="https://github.com/HmyBmny/hexo-theme-video_cn">hexo-tag-video_cn</a>目前只能插入优酷以及腾讯的视频，其它的暂不支持，谢谢！</font></center>'
+    }
+    return tag
+});
